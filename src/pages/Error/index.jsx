30b23bf9d1e04a0ac205928969de0f43fe3f1397
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2023-04-26 17:38:27 +0200

    Update error page

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,16 @@
+import './error.scss'
+import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
+
+export default function Error () {
+    return (
+        <div className='error'>
+            <Header />
+            <div className='error_infos'>
+                <h1 className='error_infos_main'>404</h1>
+                <p className='error_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
+            <Link className='error_infos_homepage' to='/Kasa/'>Retourner sur la page d’accueil</Link>
+        </div>
+    )
+}
