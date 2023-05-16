62a9052f8ab7f05c5598fb17c5c20395798ce3ff
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2023-05-16 11:26:18 +0200

    Added comments

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -3,16 +3,25 @@ import RightArrow from "../../assets/right_arrow_carousel.svg";
 import LeftArrow from "../../assets/left_arrow_carousel.svg";
 import { useState } from "react";
 
+/**
+ * Create the index, using the number of images given in the data file
+ * Define the functions in charge of changing the index when the picture changes
+ * Map the images to display them one by one
+ * Changes the opacity of the images to have a smooth transition
+ * Add the arrows and the index count if there is more than one picture
+ * @param {URL} imageSlide 
+ * @returns 
+ */
 export default function Slide({ imageSlide }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const length = imageSlide.length;
 
-  const nextSlide = () => {
-    setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
-  };
-
   const previousSlide = () => {
     setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1);
   };
 
   return (
