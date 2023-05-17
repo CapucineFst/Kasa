85bc9d0eb1642e3924e01a9dc7bdce677f5047b1
CapucineFst <99402521+CapucineFst@users.noreply.github.com>
2023-05-17 10:37:15 +0200

    Latest update

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -5,11 +5,11 @@ import greyStar from "../../assets/grey_star.svg";
  * Create an array of 5 elements and map it
  * Get the index of each element and add 1 and name it rating value
  * If the rating value is lower than or equal to the rating of the host, display a red star, otherwise display a grey star
- * @param {string} rating 
- * @returns 
+ * @param {string} rating
+ * @returns
  */
 export default function Rating({ rating }) {
-  return ([...Array(5)].map((star, index) => {
+  return [...Array(5)].map((star, index) => {
     const ratingValue = index + 1;
     return (
       <img
@@ -18,5 +18,5 @@ export default function Rating({ rating }) {
         alt="star"
       />
     );
-  }));
+  });
 }
